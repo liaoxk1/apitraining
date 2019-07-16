55f3ab542bc948dfbc1daaa598cec5ec171cf2f7
unknown <liaoxk1@gmail.com>
2019-07-16 17:18:56 +0800

    workshop02

--- workshop02/main.js
+++ workshop02/main.js
@@ -13,8 +13,8 @@ console.info(`Using ${keys.mongo}`);
 
 const db = CitiesDB({  
 	connectionUrl: keys.mongo, 
-	databaseName: 'zips', 
-	collectionName: 'city'
+	databaseName: 'cities', 
+	collectionName: 'cities'
 });
 
 const app = express();
@@ -26,17 +26,75 @@ app.use(express.urlencoded({ extended: true }));
 
 // Mandatory workshop
 // TODO GET /api/states
-
+app.get('/api/states', 
+	(req, resp) => {
+		resp.type('application/json')
+		db.findAllStates()
+		.then(result => {
+			resp.status(200)
+			resp.set('X-Date', (new Date()).toUTCString())
+			resp.json(result);
+		})
+		.catch(error => {
+			resp.status(400)
+			resp.json({ error: error})
+		});
+	}
+);
 
 
 
 // TODO GET /api/state/:state
-
+app.get('/api/state/:state',
+	(req, resp) => {
+		const stateAbbrev = req.params.state;
+		resp.type('application/json')
+		db.findAllStates()
+		.then(result => {
+			if(result.indexOf(stateAbbrev.toUpperCase()) < 0) {
+				resp.status(400);
+				resp.json({error: `Not a valid state: ${stateAbbrev}`})
+				return;
+			}
+			return db.findCitiesByState(stateAbbrev)
+		})
+		.then(result => {
+			resp.status(200)
+			resp.json(result.map(v => `/api/city/${v}`));
+		})
+		.catch(error => {
+			resp.status(400)
+			resp.json({ error: error })
+		});
+	}
+);
 
 
 
 // TODO GET /api/city/:cityId
-
+app.get('/api/city/:cityId',
+	(req, resp) => {
+		const cityId = req.params.cityId;
+		resp.type('application/json')
+		db.findCityById(cityId)
+		.then(result => {
+			if(result.length == 0) {
+				resp.status(404);
+				resp.json({error: `Not a valid city Id: ${cityId}`})
+				return;
+			}
+			return result;
+		})
+		.then(result => {
+			resp.status(200)
+			resp.json(result[0]);
+		})
+		.catch(error => {
+			resp.status(400)
+			resp.json({ error: error })
+		});
+	}
+);
 
 
 // TODO POST /api/city
